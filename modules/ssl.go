55869bc72857de3ab35646375ede7d482a1d8bcc
Yuksel Saliev <yuksel.saliev@gmail.com>
2017-04-07 14:33:32 +0300

    [WIP] SSL

--- modules/ssl.go
+++ modules/ssl.go
@@ -75,28 +75,24 @@ func letsEncryptInit(Hostname string) {
 
 	get_external("https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem","/ssl/lets-encrypt-x3-cross-signed.pem")
 
+	out, er = exec.Command("cat", []string{"/ssl/signed.crt","/ssl/intermediate.pem" }...).Output()
+	if er !=nil {
+		log.Println(er)
+	}
+	ioutil.WriteFile("/ssl/chained.pem", out, os.ModePerm)
+
+	// /etc/dovecot/dovecot.pem
 
 
 
+	exec.Command("cp", []string{"/ssl/chained.pem","/etc/dovecot/dovecot.pem" }...).Output()
+	exec.Command("cp", []string{"/ssl/domain.key","/etc/dovecot/private/dovecot.pem" }...).Output()
 
-	//// generate ssl certf
-	//commands := []cmds{}
-	//
-	////commands = append(commands, cmds{Program: "openssl", Command: []string{"req", "-new", "-sha256", "-key", "/ssl/domain.key", "-subj", "/CN=" + Hostname, ">", "/ssl/domain.csr"}})
-	////commands = append(commands, cmds{Program: "service", Command: []string{"nginx", "reload"}})
-	////commands = append(commands, cmds{Program: "python", Command: []string{"/ssl/acme_tiny.py", "--account-key", "/ssl/account.key", "--csr", "/ssl/domain.csr", "--acme-dir", "/var/www/challenges/", ">", "/ssl/signed.crt"}})
-	////commands = append(commands, cmds{Program: "wget", Command: []string{"-O", "-", "https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem", ">", "/ssl/intermediate.pem"}})
-	////commands = append(commands, cmds{Program: "cat", Command: []string{"signed.crt", "/ssl/intermediate.pem", ">", "/ssl/chained.pem"}})
-	//
-	//for el := range commands {
-	//	log.Println( commands[el].Program )
-	//	log.Println( commands[el].Command )
-	//	out, er := exec.Command(commands[el].Program, commands[el].Command...).Output()
-	//	if er != nil {
-	//		log.Println(er)
-	//
-	//	}
-	//	log.Println(string(out))
-	//}
+	out, er = exec.Command("service", []string{"nginx","restart" }...).Output()
+	if er !=nil {
+		log.Println(er)
+	} else {
+		log.Println(string(out))
+	}
 
 }
