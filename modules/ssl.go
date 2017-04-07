a7f4fbdba2139f4b06db5739135eb95081acb74b
Yuksel Saliev <yuksel.saliev@gmail.com>
2017-04-07 11:43:43 +0300

    [WIP] - lets-encrypt

--- modules/ssl.go
+++ modules/ssl.go
@@ -34,12 +34,12 @@ func letsEncryptInit(Hostname string) {
 	commands := []cmds{}
 
 	commands = append(commands, cmds{Program: "openssl", Command: []string{"genrsa", "4096", ">", "/ssl/account.key"}})
-	commands = append(commands, cmds{Program: "openssl", Command: []string{"genrsa", "4096", ">", "/ssl/domain.key"}})
-	commands = append(commands, cmds{Program: "openssl", Command: []string{"req", "-new", "-sha256", "-key", "/ssl/domain.key", "-subj", "/CN=" + Hostname, ">", "/ssl/domain.csr"}})
-	commands = append(commands, cmds{Program: "service", Command: []string{"nginx", "reload"}})
-	commands = append(commands, cmds{Program: "python", Command: []string{"/ssl/acme_tiny.py", "--account-key", "/ssl/account.key", "--csr", "/ssl/domain.csr", "--acme-dir", "/var/www/challenges/", ">", "/ssl/signed.crt"}})
-	commands = append(commands, cmds{Program: "wget", Command: []string{"-O", "-", "https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem", ">", "/ssl/intermediate.pem"}})
-	commands = append(commands, cmds{Program: "cat", Command: []string{"signed.crt", "/ssl/intermediate.pem", ">", "/ssl/chained.pem"}})
+	//commands = append(commands, cmds{Program: "openssl", Command: []string{"genrsa", "4096", ">", "/ssl/domain.key"}})
+	//commands = append(commands, cmds{Program: "openssl", Command: []string{"req", "-new", "-sha256", "-key", "/ssl/domain.key", "-subj", "/CN=" + Hostname, ">", "/ssl/domain.csr"}})
+	//commands = append(commands, cmds{Program: "service", Command: []string{"nginx", "reload"}})
+	//commands = append(commands, cmds{Program: "python", Command: []string{"/ssl/acme_tiny.py", "--account-key", "/ssl/account.key", "--csr", "/ssl/domain.csr", "--acme-dir", "/var/www/challenges/", ">", "/ssl/signed.crt"}})
+	//commands = append(commands, cmds{Program: "wget", Command: []string{"-O", "-", "https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem", ">", "/ssl/intermediate.pem"}})
+	//commands = append(commands, cmds{Program: "cat", Command: []string{"signed.crt", "/ssl/intermediate.pem", ">", "/ssl/chained.pem"}})
 
 	for el := range commands {
 		log.Println( commands[el].Program )
