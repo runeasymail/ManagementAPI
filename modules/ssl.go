fd5689bec9cf370250947d9dc9c89ecf76100f40
Yuksel Saliev <yuksel.saliev@gmail.com>
2017-04-07 14:08:41 +0300

    [WIP] - lets-encrypt

--- modules/ssl.go
+++ modules/ssl.go
@@ -30,26 +30,33 @@ func letsEncryptInit(Hostname string) {
 	// mkdir -p /var/www/challenges/
 	os.MkdirAll("/var/www/challenges/", os.ModePerm)
 
-	// generate ssl certf
-	commands := []cmds{}
 
-	commands = append(commands, cmds{Program: "openssl", Command: []string{"genrsa", "4096"}})
-	//commands = append(commands, cmds{Program: "openssl", Command: []string{"genrsa", "4096", ">", "/ssl/domain.key"}})
-	//commands = append(commands, cmds{Program: "openssl", Command: []string{"req", "-new", "-sha256", "-key", "/ssl/domain.key", "-subj", "/CN=" + Hostname, ">", "/ssl/domain.csr"}})
-	//commands = append(commands, cmds{Program: "service", Command: []string{"nginx", "reload"}})
-	//commands = append(commands, cmds{Program: "python", Command: []string{"/ssl/acme_tiny.py", "--account-key", "/ssl/account.key", "--csr", "/ssl/domain.csr", "--acme-dir", "/var/www/challenges/", ">", "/ssl/signed.crt"}})
-	//commands = append(commands, cmds{Program: "wget", Command: []string{"-O", "-", "https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem", ">", "/ssl/intermediate.pem"}})
-	//commands = append(commands, cmds{Program: "cat", Command: []string{"signed.crt", "/ssl/intermediate.pem", ">", "/ssl/chained.pem"}})
 
-	for el := range commands {
-		log.Println( commands[el].Program )
-		log.Println( commands[el].Command )
-		out, er := exec.Command(commands[el].Program, commands[el].Command...).Output()
-		if er != nil {
-			log.Println(er)
+	out, _ := exec.Command("openssl", []string{"genrsa", "4096"}...).Output()
 
-		}
-		log.Println(string(out))
-	}
+	log.Println(out)
+
+
+	//// generate ssl certf
+	//commands := []cmds{}
+	//
+	//commands = append(commands, cmds{Program: "openssl", Command: []string{"genrsa", "4096"}})
+	////commands = append(commands, cmds{Program: "openssl", Command: []string{"genrsa", "4096", ">", "/ssl/domain.key"}})
+	////commands = append(commands, cmds{Program: "openssl", Command: []string{"req", "-new", "-sha256", "-key", "/ssl/domain.key", "-subj", "/CN=" + Hostname, ">", "/ssl/domain.csr"}})
+	////commands = append(commands, cmds{Program: "service", Command: []string{"nginx", "reload"}})
+	////commands = append(commands, cmds{Program: "python", Command: []string{"/ssl/acme_tiny.py", "--account-key", "/ssl/account.key", "--csr", "/ssl/domain.csr", "--acme-dir", "/var/www/challenges/", ">", "/ssl/signed.crt"}})
+	////commands = append(commands, cmds{Program: "wget", Command: []string{"-O", "-", "https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem", ">", "/ssl/intermediate.pem"}})
+	////commands = append(commands, cmds{Program: "cat", Command: []string{"signed.crt", "/ssl/intermediate.pem", ">", "/ssl/chained.pem"}})
+	//
+	//for el := range commands {
+	//	log.Println( commands[el].Program )
+	//	log.Println( commands[el].Command )
+	//	out, er := exec.Command(commands[el].Program, commands[el].Command...).Output()
+	//	if er != nil {
+	//		log.Println(er)
+	//
+	//	}
+	//	log.Println(string(out))
+	//}
 
 }
