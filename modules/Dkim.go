3ec37e241a97973ddc50035131cacbea6062361c
Yuksel Saliev <yuksel.saliev@gmail.com>
2017-05-01 12:51:13 +0300

    [WIP]

--- modules/Dkim.go
+++ modules/Dkim.go
@@ -29,40 +29,28 @@ func add(Domain string) {
 
 	f, _ := ioutil.ReadFile(trusted_host)
 	content := string(f)
-	content = fmt.Sprintf("%s \n %s", content, "*"+Domain )
+	content = fmt.Sprintf("%s \n %s \n", content, "*"+Domain )
 
 	log.Debug("Original ", trusted_host, "content: ", content)
 
 	ioutil.WriteFile(trusted_host, []byte(content), os.ModePerm)
 
+
+	f, _ = ioutil.ReadFile(keytable)
+	content = string(f)
+	log.Debug("Original ", keytable, "content: ", content)
+	content = fmt.Sprintf("%s \n %s \n", content, fmt.Sprintf("mail._domainkey.%s %s:mail:/etc/opendkim/keys/%s/mail.private", Domain, Domain, Domain) )
+	ioutil.WriteFile(keytable, []byte(content), os.ModePerm)
+
+
+	f, _ = ioutil.ReadFile(signinTable)
+	content = string(f)
+	log.Debug("Original ", signinTable, "content: ", content)
+	content = fmt.Sprintf("%s \n %s \n", content, fmt.Sprintf("*@%s mail._domainkey.%s", Domain, Domain) )
+	ioutil.WriteFile(signinTable, []byte(content), os.ModePerm)
+
+
 	
-
-
-
-	//f, er := os.OpenFile(trusted_host, os.O_APPEND, 0666)
-	//if er != nil {
-	//	log.Println(er)
-	//}
-	//
-	//_, er = f.WriteString("*" + Domain)
-	//if er != nil {
-	//	log.Println(er)
-	//}
-	//er = f.Close()
-	//if er != nil {
-	//	log.Println(er)
-	//}
-	//
-	//
-	//
-	//f, _ = os.OpenFile(keytable, os.O_APPEND, 0666)
-	//f.WriteString(fmt.Sprintf("mail._domainkey.%s %s:mail:/etc/opendkim/keys/%s/mail.private", Domain, Domain, Domain))
-	//f.Close()
-	//
-	//f, _ = os.OpenFile(signinTable, os.O_APPEND, 0666)
-	//f.WriteString(fmt.Sprintf("*@%s mail._domainkey.%s", Domain, Domain))
-	//f.Close()
-
 	// create dir
 	os.MkdirAll("/etc/opendkim/keys/"+Domain, os.ModePerm)
 
